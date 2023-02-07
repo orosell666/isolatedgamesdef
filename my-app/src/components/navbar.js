81f467f48f139537c389f11f76d81fb654ce8000
orosell666 <o.rosell.ribas@gmail.com>
2023-02-07 10:03:03 +0000

    navbar

---
+++ my-app/src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Navbar = () => {
+
+    return (
+        <nav className="navbar">
+            <p>hello moderfucker</p>
+        </nav>
+    )
+}
+export default Navbar;
