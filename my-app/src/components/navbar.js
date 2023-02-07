e7313589828a17fd44c4b8cf3454c98c376efe80
orosell666 <o.rosell.ribas@gmail.com>
2023-02-07 11:27:02 +0000

    bit front

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -5,8 +5,14 @@ import React from "react";
 const Navbar = () => {
 
     return (
-        <nav className="navbar">
-            <p>hello moderfucker</p>
+        <nav className="navbar navstyle">
+            <div className="container">
+                <img src="" />
+            </div>
+            <p>logo</p>
+            <p>new</p>
+            <p>jobs</p>
+            <p>contact</p>
         </nav>
     )
 }
