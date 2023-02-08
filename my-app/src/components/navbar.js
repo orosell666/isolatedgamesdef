ab50ab67bf55118cde2ee5386f2184a05b834149
orosell666 <o.rosell.ribas@gmail.com>
2023-02-08 11:35:31 +0000

    navbar

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -7,25 +7,28 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 
     return (
-        <nav className="navbar navstyle">
+        <nav className="navbar navstyle ">
             <div className="container">
-                <img src="" />
-            </div >
-            <div className="btn-group" role="group" >
+                
+
             <Link to="/">
                 
-            <button type="button" className="btn btn-dark">HOME</button>
+            <img className="navbar-brand mb-0 h1" src="//www.isolatedgames.com/wp-content/uploads/2017/02/Logo-IsolatedGames-1.png" />
                
             </Link>
-            <Link to="/contact.js">
-                <button type="button" className="btn btn-dark">CONTACT</button>
-            </Link>
-            <Link to="/jobs.js">
-                <button type="button" className="btn btn-dark">JOBS</button>
-            </Link>
-            <Link to="/news.js">
-                <button type="button" className="btn btn-dark">NEWS</button>
-            </Link></div>
+                    <div className="btn-group me-5" role="group" >
+                    <Link to="/contact.js">
+                        <button type="button" className=" navbar-toggler btn btn-dark">CONTACT</button>
+                    </Link>
+                    <Link to="/jobs.js">
+                        <button type="button" className="navbar-toggler btn btn-dark">JOBS</button>
+                    </Link>
+                    <Link to="/news.js">
+                        <button type="button" className="navbar-toggler btn btn-dark">NEWS</button>
+                    </Link>
+                    </div>
+                </div>
+            
         </nav>
     )
 }
