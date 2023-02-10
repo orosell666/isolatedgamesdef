5653dc09f67c4c8e6bc0081a737d68631b2b8ac7
orosell666 <o.rosell.ribas@gmail.com>
2023-02-10 13:53:21 +0000

    arreglo navbar y cards

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -7,20 +7,22 @@ import LogoIG from "../logoIsolatedGames.png";
 const Navbar = () => {
 
     return (
-        <nav className="navbar navstyle sticky-top">
-            <div className="container">
-                
-
-            
-            <a type="button" className="navbar-toggler btn btn-dark" href="#scrollspyHeading1">
-            <img className="navbar-brand mb-0 h1" src={LogoIG} width={220} height={60}/></a> 
-               
-            
-                    <div className="btn-group me-5" role="group" >
-                    <a type="button" className="navbar-toggler btn btn-dark" href="#scrollspyHeading4">CONTACT</a>
-                    <a type="button" className="navbar-toggler btn btn-dark" href="#scrollspyHeading3">JOBS</a>
-                    
-                        <a type="button" className="navbar-toggler btn btn-dark" href="#scrollspyHeading2">NEWS</a>
+        <nav className="navbar navbar-expand-lg  navstyle navbar-dark  sticky-top"> 
+            <div className="container-fluid">
+                <a  href="#scrollspyHeading1">
+            <img className="navbar-brand ms-5" src={LogoIG} width={220} height={60}/></a> 
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+               <div className="collapse navbar-collapse justify-content-end me-5" id="navbarTogglerDemo02" >
+                        <ul className="navbar-nav ">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#scrollspyHeading4">CONTACT</a></li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#scrollspyHeading3">JOBS</a></li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#scrollspyHeading2">NEWS</a></li>
+                        </ul>
                     
                     </div>
                 </div>
--- my-app/src/components/news.js
+++ my-app/src/components/news.js
@@ -11,52 +11,52 @@ const News = () => {
             <div className="container justify-content-start">
             <div className="card bg-dark mt-5">
                 <div className="row g-0">
-                 <div className="col-8">
+                 <div className="col-sm-8">
                 <div className="card-body text-start">
                     <h3 className="card-title">New version for the game</h3>
                     <small className="text-muted"><strong>MAJOR UPDATE</strong> FEB 15, 2023</small>
                     <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div></div>  
-                <div className="col-4">
+                <div className="col-sm-4">
                 <img src={screenshot} className="img-fluid" /></div>
                 
                 </div>
             </div>
             <div className="card bg-dark mt-2">
                 <div className="row g-0">
-                 <div className="col-8">
+                 <div className="col-sm-8">
                 <div className="card-body text-start">
                     <h3 className="card-title">New version for the game</h3>
                     <small className="text-muted"><strong>MAJOR UPDATE</strong> FEB 15, 2023</small>
                     <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div></div>  
-                <div className="col-4">
+                <div className="col-sm-4">
                 <img src={screenshot2} className="img-fluid" /></div>
                 
                 </div>
             </div>
             <div className="card bg-dark mt-2">
                 <div className="row g-0">
-                 <div className="col-8">
+                 <div className="col-sm-8">
                 <div className="card-body text-start">
                     <h3 className="card-title">New version for the game</h3>
                     <small className="text-muted"><strong>MAJOR UPDATE</strong> FEB 15, 2023</small>
                     <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div></div>  
-                <div className="col-4">
+                <div className="col-sm-4">
                 <img src={screenshot3} className="img-fluid" /></div>
                 
                 </div>
             </div>
             <div className="card bg-dark mt-2">
                 <div className="row g-0">
-                 <div className="col-8">
+                 <div className="col-sm-8">
                 <div className="card-body text-start">
                     <h3 className="card-title">New version for the game</h3>
                     <small className="text-muted"><strong>MAJOR UPDATE</strong> FEB 15, 2023</small>
                     <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div></div>  
-                <div className="col-4">
+                <div className="col-sm-4">
                 <img src={screenshot3} className="img-fluid" /></div>
                 
                 </div>
