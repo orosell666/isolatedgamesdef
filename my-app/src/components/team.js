c59cb21733d4dd2030894649758a2c4749ab3971
Oriol Rosell <o.rosell.ribas@gmail.com>
2023-04-01 07:57:01 +0000

    more front

--- my-app/src/components/team.js
+++ my-app/src/components/team.js
@@ -6,14 +6,15 @@ const Team=()=>{
     return(
         <div className="textColor1  top-0 text-center teambg">
             <h1 className=" mb-5 pt-5 ">TEAM</h1>
-            <div className="container col-10 ">
-                <div className="row align-items-start">
+            <div className="container col-8 ">
+                
+                <div className="row row-cols-2 row-cols-md-3 g-4 pb-5">
+                    
                     <div className="col">
-                        
-                        <div className="card  rounded-3 w-100">
+                        <div className="card col-3 rounded-3 w-100 h-100">
                             <div className="row g-0">
                                     <div className=" d-flex">
-                                    <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
+                                    <img src={Avatar} className="d-none d-sm-block  col-sm-3  p-2" />
                                     <div className="card-body ">
                                     <h4 className="card-header border-0"><ins>Francesc Romero</ins></h4>
                                         <p className="card-text  text-center">CEO</p>
@@ -21,10 +22,11 @@ const Team=()=>{
                                     </div>
                             </div>
                         </div>
+                        </div>
 
-                    </div>
-                    <div className="col">
-                        <div className="card rounded-3 w-100">
+                   
+                        <div className="col">
+                        <div className="card col-3 rounded-3 w-100 h-100">
                             <div className="row g-0">
                                     <div className=" d-flex">
                                     <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
@@ -34,11 +36,11 @@ const Team=()=>{
                                         </div> 
                                     </div>
                             </div>
-                        </div>
+                        </div></div>
 
-                    </div>
-                    <div className="col">
-                        <div className="card  rounded-3 w-100">
+                    
+                        <div className="col">
+                        <div className="card col-3 rounded-3 w-100 h-100">
                             <div className="row g-0">
                                     <div className=" d-flex">
                                     <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
@@ -48,13 +50,13 @@ const Team=()=>{
                                         </div> 
                                     </div>
                             </div>
-                        </div>
+                        </div></div>
 
-                    </div>
-                </div>
-                <div className="row align-items-center mt-4">
-                    <div className="col">
-                        <div className="card  rounded-3 w-100">
+                    
+                
+                
+                        <div className="col">
+                        <div className="card col-3 rounded-3 w-100 h-100">
                             <div className="row g-0">
                                     <div className=" d-flex">
                                     <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
@@ -63,12 +65,12 @@ const Team=()=>{
                                         <p className="card-text  text-center">Pixel Artist</p>
                                         </div> 
                                     </div>
-                            </div>
-                        </div>
+                            </div></div></div>
+                        
 
-                    </div>
-                    <div className="col">
-                        <div className="card   rounded-3 w-100">
+                   
+                            <div className="col">
+                        <div className="card col-3  rounded-3 w-100 h-100">
                             <div className="row g-0">
                                     <div className=" d-flex">
                                     <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
@@ -78,11 +80,11 @@ const Team=()=>{
                                         </div> 
                                     </div>
                             </div>
-                        </div>
+                        </div></div>
 
-                    </div>
-                    <div className="col">
-                        <div className="card   rounded-3 w-100">
+                    
+                        <div className="col">
+                        <div className="card col-3  rounded-3 w-100 h-100">
                             <div className="row g-0">
                                     <div className=" d-flex">
                                     <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
@@ -92,13 +94,13 @@ const Team=()=>{
                                         </div> 
                                     </div>
                             </div>
-                        </div>
+                        </div></div>
 
-                    </div>
-                </div>
-                <div className="row align-items-end mt-4 pb-5">
-                    <div className="col">
-                        <div className="card   rounded-3 w-100">
+                   
+                
+                
+                        <div className="col">
+                        <div className="card col-3  rounded-3 w-100 h-100">
                             <div className="row g-0">
                                     <div className=" d-flex">
                                     <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
@@ -108,11 +110,26 @@ const Team=()=>{
                                         </div> 
                                     </div>
                             </div>
-                        </div>
+                      
 
-                    </div>
+                    </div></div>
                     <div className="col">
-                        <div className="card   rounded-3 w-100">
+                        <div className="card col-3  rounded-3 w-100 h-100">
+                            <div className="row g-0">
+                                    <div className=" d-flex">
+                                    <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
+                                    <div className="card-body ">
+                                    <h4 className="card-header border-0"><ins>Sabrina Purswani</ins></h4>
+                                        <p className="card-text  text-center">Programmer</p>
+                                    
+                                        </div> 
+                                    </div>
+                            </div>
+                        </div></div>
+
+                    
+                        <div className="col">
+                        <div className="card col-3  rounded-3 w-100 h-100">
                             <div className="row g-0">
                                     <div className=" d-flex">
                                     <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
@@ -124,23 +141,9 @@ const Team=()=>{
                             </div>
                         </div>
 
-                    </div>
-                    <div className="col">
-                        <div className="card   rounded-3 w-100">
-                            <div className="row g-0">
-                                    <div className=" d-flex">
-                                    <img src={Avatar} className="d-none d-sm-block col-sm-3  p-2" />
-                                    <div className="card-body ">
-                                    <h4 className="card-header border-0"><ins>Francesc Romero</ins></h4>
-                                        <p className="card-text  text-center">Supreeme Master Paychecks </p>
-                                        </div> 
-                                    </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
+                        </div> 
+                
+</div>
             </div>
         </div>
 
